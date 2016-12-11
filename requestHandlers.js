45028697e3a97681eff692f3f13fe907c18ffb32
Zhiwei Yang <yangzhiwei@youzan.com>
2016-12-11 11:56:17 +0800

    让router去处理response

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,10 +1,22 @@
-function start() {
+var exec = require("child_process").exec;
+
+function start(response) {
     console.log("Request handler 'start' was called");
+    exec("ls -lah",function (error,stdout,stderr) {
+        response.writeHead(200,{"Content-Type":"text/plain"});
+        response.write(stdout);
+        response.write(stderr);
+        response.end();
+    });
 
 }
 
-function upload() {
+function upload(response) {
     console.log("request handeler 'upload' was called");
+
+    response.writeHead(200,{"Content-Type":"text/plain"});
+    response.write("Hello Upload");
+    response.end();
 
 }
 
--- server2.js
+++ server2.js
@@ -1,16 +1,16 @@
-var http = require("http")
-var url = require("url")
+var http = require("http");
+var url = require("url");
 function start(route,handle) {
     function onRequest(request,response) {
         var pathname = url.parse(request.url).pathname;
         console.log("Request" + pathname + "received");
 
-        route(handle,pathname);
+        route(handle,pathname,response);
 
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("Hello World");
-        response.end();
-    
+        // response.writeHead(200, {"Content-Type": "text/plain"});
+        // response.write("Hello World");
+        // response.end();
+        //
 }
     http.createServer(onRequest).listen(8889);
     console.log("Server has started.");
