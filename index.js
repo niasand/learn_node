fa7d439438e3ce707073a13f25704397f341826f
Zhiwei Yang <yangzhiwei@youzan.com>
2016-12-11 12:31:25 +0800

    处理post的全部内容

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ var server = require("./server2");
 var router = require("./router");
 var requestHandlers = require("./requestHandlers");
 
-var handle = {}
+var handle = {};
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
 handle["/favicon.ico"] = requestHandlers.favicon.ico;
+handle["/start_v1"] = requestHandlers.start_v1;
 
 server.start(router.route,handle);
--- requestHandlers.js
+++ requestHandlers.js
@@ -1,4 +1,5 @@
 var exec = require("child_process").exec;
+var querystring = require("querystring");
 
 function start(response) {
     console.log("Request handler 'start' was called");
@@ -11,11 +12,31 @@ function start(response) {
 
 }
 
-function upload(response) {
+function start_v1(response,postData){
+    console.log("Requese Handler 'end' was called");
+
+    var body = '<html>'+
+        '<head>'+
+        '<meta http-equiv="Content-Type" content="text/html; '+
+        'charset=UTF-8" />'+
+        '</head>'+
+        '<body>'+
+        '<form action="/upload" method="post">'+
+        '<textarea name="text" rows="20" cols="60"></textarea>'+
+        '<input type="submit" value="Submit text" />'+
+        '</form>'+
+        '</body>'+
+        '</html>';
+    response.writeHead(200,{"Content-Type":"text/html"});
+    response.write(body);
+    response.end();
+}
+
+function upload(response,postData) {
     console.log("request handeler 'upload' was called");
 
     response.writeHead(200,{"Content-Type":"text/plain"});
-    response.write("Hello Upload");
+    response.write("Hello Upload,you have sent this message: "+querystring.parse(postData).text);
     response.end();
 
 }
@@ -29,3 +50,4 @@ function favicon() {
 exports.start = start;
 exports.upload = upload;
 exports.favicon = favicon;
+exports.start_v1 = start_v1;
